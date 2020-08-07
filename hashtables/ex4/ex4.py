bbf059594d267935b62c1a96e07ac9d8f4d3fa25
Liam Moore <liam.moore.dev@gmail.com>
2020-08-07 16:58:12 +0000

    completes exercise 4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,14 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    table = {}
+    result_table = {}
+    for num in a:
+        if (num * -1) in table:
+            result_table[abs(num)] = abs(num)
+        table[num] = True
 
+    result = list(result_table.values())
     return result
 
 
